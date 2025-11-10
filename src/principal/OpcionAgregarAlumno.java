1ea82cdf29e95156b53719404c4b630be8f89e47
OihanGonzalez <gonzalezoihan8@gmail.com>
2025-11-10 13:21:31 +0100

    Añadida la clase OpcionAgregarAlumno

---
+++ src/principal/OpcionAgregarAlumno.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class OpcionAgregarAlumno implements IOpcionMenu {
+    @Override
+    public void ejecutar(GestorAlumnos gestorAlumnos) {
+        // Solicitar los datos del alumno y agregarlo
+        Alumno nuevoAlumno = new Alumno("Juan", 20);
+        gestorAlumnos.agregarAlumno(nuevoAlumno);
+    }
+}
